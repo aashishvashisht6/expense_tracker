979111a2617a7d37b5d2b6ff1f1565519c21e826
aashishvashisht6 <aashishvashisht6@gmail.com>
2026-02-21 03:12:25 +0530

    fix: package json & vite config file

--- my_expense/vite.config.ts
+++ my_expense/vite.config.ts
@@ -1,41 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from 'vite-plugin-pwa'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/assets/expense_tracker/my_expense/',
-  plugins: [react(),
-  VitePWA({
-    registerType: "autoUpdate",
-    includeAssets: ['favicon.ico', 'robots.txt'],
-    outDir: "../expense_tracker/public/my_expense",
-    manifest: {
-      name: "my_expense",
-      short_name: "Expense",
-      description: "My Expense Tracker",
-      start_url: '/my_expense/',      // must be in scope
-      scope: '/my_expense/',          // explicitly set scope
-      display: "standalone",
-      background_color: "#ffffff",
-      theme_color: "#4caf50",
-      "icons": [
-        {
-          src: '/assets/expense_tracker/my_expense/vite.svg',
-          sizes: '192x192',
-          type: 'image/svg'
-        },
-        {
-          src: '/assets/expense_tracker/my_expense/vite.svg',
-          sizes: '512x512',
-          type: 'image/svg'
-        }
-      ],
-    }
-  })
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
